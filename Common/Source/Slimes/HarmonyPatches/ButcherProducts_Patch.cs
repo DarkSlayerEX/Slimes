885ce011e7a3816ff25c0849487d0d43a28baaa4
Taranchuk <nfhfyxer@gmail.com>
2020-10-21 19:04:37 +0300

    update

--- Common/Source/Slimes/HarmonyPatches/ButcherProducts_Patch.cs
+++ Common/Source/Slimes/HarmonyPatches/ButcherProducts_Patch.cs
@@ -30,32 +30,46 @@ namespace Slimes
 
         private static List<Thing> GenerateExtraButcherProducts(List<Corpse> corpseSlimes)
         {
+            Log.Message(" - GenerateExtraButcherProducts - var things = new List<Thing>(); - 1", true);
             var things = new List<Thing>();
+            Log.Message(" - GenerateExtraButcherProducts - foreach (var corpse in corpseSlimes) - 2", true);
             foreach (var corpse in corpseSlimes)
             {
+                Log.Message(" - GenerateExtraButcherProducts - var butcherComp = corpse.InnerPawn.GetComp<CompExtraButcherProducts>(); - 3", true);
                 var butcherComp = corpse.InnerPawn.GetComp<CompExtraButcherProducts>();
+                Log.Message(" - GenerateExtraButcherProducts - if (butcherComp != null) - 4", true);
                 if (butcherComp != null)
                 {
+                    Log.Message(" - GenerateExtraButcherProducts - if (butcherComp.Props.butcherSpecialized.oneOf != null && butcherComp.Props.butcherSpecialized.oneOf.Count > 0) - 5", true);
                     if (butcherComp.Props.butcherSpecialized.oneOf != null && butcherComp.Props.butcherSpecialized.oneOf.Count > 0)
                     {
+                        Log.Message(" - GenerateExtraButcherProducts - var butcherData = butcherComp.Props.butcherSpecialized.oneOf.RandomElement(); - 6", true);
                         var butcherData = butcherComp.Props.butcherSpecialized.oneOf.RandomElement();
+                        Log.Message(" - GenerateExtraButcherProducts - Thing thing = null; - 7", true);
                         Thing thing = null;
+                        Log.Message(" - GenerateExtraButcherProducts - if (butcherData.yieldRange != null) - 8", true);
                         if (butcherData.yieldRange != null)
                         {
+                            Log.Message(" - GenerateExtraButcherProducts - thing = ThingMaker.MakeThing(butcherData.thingDef); - 9", true);
                             thing = ThingMaker.MakeThing(butcherData.thingDef);
+                            Log.Message(" - GenerateExtraButcherProducts - thing.stackCount = butcherData.yieldRange.RandomInRange; - 10", true);
                             thing.stackCount = butcherData.yieldRange.RandomInRange;
                         }
                         else if (butcherData.baseYield > 0)
                         {
+                            Log.Message(" - GenerateExtraButcherProducts - thing = ThingMaker.MakeThing(butcherData.thingDef); - 12", true);
                             thing = ThingMaker.MakeThing(butcherData.thingDef);
+                            Log.Message(" - GenerateExtraButcherProducts - thing.stackCount = butcherData.baseYield; - 13", true);
                             thing.stackCount = butcherData.baseYield;
                         }
+                        Log.Message(" - GenerateExtraButcherProducts - if (thing != null) - 14", true);
                         if (thing != null)
                         {
+                            Log.Message(" - GenerateExtraButcherProducts - things.Add(thing); - 15", true);
                             things.Add(thing);
                         }
                     }
-                    else if (butcherComp.Props.butcherSpecialized.allOf != null && butcherComp.Props.butcherSpecialized.allOf.Count > 0)
+                    if (butcherComp.Props.butcherSpecialized.allOf != null && butcherComp.Props.butcherSpecialized.allOf.Count > 0)
                     {
                         foreach (var butcherData in butcherComp.Props.butcherSpecialized.allOf)
                         {
